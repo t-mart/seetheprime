2fa8c36e297c38e384e02152dd27d0d37e4a941e
Tim Martin <tim@timmart.in>
2024-11-12 22:31:51 -0600

    Add plausible

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 	import '../app.css';
+	import { PUBLIC_PLAUSIBLE_DOMAIN, PUBLIC_PLAUSIBLE_SOURCE_URL } from '$env/static/public';
+
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<script defer data-domain={PUBLIC_PLAUSIBLE_DOMAIN} src={PUBLIC_PLAUSIBLE_SOURCE_URL}></script>
+</svelte:head>
 
 {@render children()}
